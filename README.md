8dd3ab8376bac9f5267fa700161b0b854c760abf
harshil0217 <harshil0217@gmail.com>
2024-08-18 18:34:25 -0400

    added loss and accuracy graph

---
+++ README.md
@@ -0,0 +1,4 @@
+# Neural Network for the Detection of Destroyed or Damaged Villages in Darfur, Sudan
+
+In this project, I train a neural network capable of detecting if a village in Darfur, Sudan has been damaged or destroyed via free and publically accessible LANDSAT7 30m resolution remote sensing data.
+
--- src/model_training.py
+++ src/model_training.py
@@ -2,14 +2,11 @@ import numpy as np
 import pandas as pd
 from data_preprocessing import preprocess_data
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-import sklearn
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 import tensorflow as tf
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
-from sklearn.preprocessing import MinMaxScaler
 
 # load the preprocessed data
 X_train, X_test, y_train, y_test = preprocess_data()
@@ -28,7 +25,9 @@ adam = keras.optimizers.Adam(learning_rate=0.001)
 model.compile(loss='binary_crossentropy', optimizer=adam, metrics=["accuracy"])
 
 #train the model
-model.fit(X_train, y_train, epochs=100)
+history = model.fit(X_train, y_train, epochs=100)
+
+
 
 #evaluate the model
 loss_and_metrics = model.evaluate(X_test, y_test)
@@ -44,3 +43,25 @@ disp = ConfusionMatrixDisplay(confusion_matrix=cm)
 disp.plot()
 plt.savefig('./images/confusion_matrix.png')
 print(classification_report(y_test, preds))
+
+#graphing the loss and accuracy
+fig, ax1 = plt.subplots()
+
+ax1.set_xlabel('Epoch')
+ax1.set_ylabel('Loss', color='red')
+ax1.plot(history.history['loss'], color='red', label = 'Loss')
+ax1.tick_params(axis='y', labelcolor='red')
+
+ax2 = ax1.twinx()
+
+ax2.set_ylabel('Accuracy', color='blue')
+ax2.plot(history.history['accuracy'], color='blue', label = 'Accuracy')
+ax2.tick_params(axis='y', labelcolor='blue')
+
+fig.tight_layout()
+fig.subplots_adjust(top=0.88)
+fig.legend(loc='upper right')
+
+plt.title('Loss and Accuracy')
+plt.savefig('./images/loss_accuracy.png')
+
